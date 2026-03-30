fbb3db3dc2e478f32bfe72d491c2699abb1c3d16
takeo-takuya-lvgs <takuya.takeo@leverages.jp>
2026-03-30 17:43:29 +0900

    lambdaからsetrets managerの値を取得するサンプルを追加

--- apps/workers/src/google-calendar-webhook-handler.ts
+++ apps/workers/src/google-calendar-webhook-handler.ts
@@ -1,4 +1,10 @@
 import { SQSEvent, SQSHandler } from 'aws-lambda'
+import {
+  SecretsManagerClient,
+  GetSecretValueCommand,
+} from '@aws-sdk/client-secrets-manager'
+
+const secretsClient = new SecretsManagerClient({})
 
 /**
  * カレンダーイベントのwebhook
@@ -7,4 +13,17 @@ import { SQSEvent, SQSHandler } from 'aws-lambda'
 export const handler: SQSHandler = async (event: SQSEvent): Promise<void> => {
   console.log('--- [google-calendar-webhook.handler] from sqs event.')
   console.log(JSON.stringify(event, null, 2))
+  try {
+    const secretValueCommand = new GetSecretValueCommand({
+      SecretId: 'api-server/internal-api-key',
+    })
+    const apiKeyResponse = await secretsClient.send(secretValueCommand)
+    if (!apiKeyResponse.SecretString) {
+      throw new Error('invalid api key...')
+    }
+    const secret = JSON.parse(apiKeyResponse.SecretString)
+    console.log(`* api_key: `, secret)
+  } catch (e) {
+    console.error(e)
+  }
 }
